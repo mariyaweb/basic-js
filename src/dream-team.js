cc63359209f442ec0117ab0eaa708b05194fa2ce
Maryia Skachkova <96mariyask@gmail.com>
2023-04-14 22:53:17 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+   let nameFirstLetter = [];
+   if (typeof (members) != 'object' || members == null) {
+      return false;
+   } else {
+      for (let i = 0; i < members.length; i++) {
+         if (typeof (members[i]) == 'string') {
+            let str = members[i].trim();
+            nameFirstLetter.push(str[0].toUpperCase());
+         }
+      }
+      return nameFirstLetter.sort().join('');
+   }
 }
 
+
 module.exports = {
-  createDreamTeam
+   createDreamTeam
 };

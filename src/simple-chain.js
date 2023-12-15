79e238d7f7eda2f07fe54309d08a08ba1b68b1d4
github-mariyaweb <96mariyask@gmail.com>
2023-12-15 15:34:03 +0300

    feat: add solution of the Chain maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.arr.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (this.arr.length < position || position < 1 || typeof position !== 'number') {
+      this.arr = [];
+      throw new Error("You can't remove incorrect link!");
+    } else {
+      this.arr.splice(position - 1, 1);
+    }
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const chain = this.arr.join('~~');
+    this.arr = [];
+    return chain;
   }
 };
 

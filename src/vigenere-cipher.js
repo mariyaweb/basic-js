8e8d3f52fdf379b639d0100ecb73c84006e90347
github-mariyaweb <96mariyask@gmail.com>
2023-12-19 17:43:29 +0300

    feat: add solution of the vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,94 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!')
+    };
+
+    let messageEnc = message.toUpperCase().split('');
+    let keyEnc = key.toUpperCase().split('');
+    let messageArr = [];
+    let keyArr = [];
+    let keyIndex = 0;
+    let res = [];
+
+    messageEnc.forEach((item, index) => {
+      if (this.alphabet.includes(item)) {
+        messageArr.push(this.alphabet.indexOf(item));
+        if (keyEnc.length - 1 <= keyIndex) {
+          keyArr.push(this.alphabet.indexOf(keyEnc[keyIndex]));
+          keyIndex = 0;
+        } else {
+          keyArr.push(this.alphabet.indexOf(keyEnc[keyIndex]));
+          keyIndex = keyIndex + 1;
+        }
+
+      } else {
+        messageArr.push(messageEnc[index]);
+        keyArr.push(messageEnc[index]);
+      }
+    })
+
+    messageArr.forEach((item, index) => {
+      if (typeof item === 'number') {
+        res.push(this.alphabet[(item + keyArr[index]) % 26])
+      } else {
+        res.push(item);
+      }
+    })
+
+    return this.direct ? res.join('') : res.reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!')
+    };
+
+    let messageDec = encryptedMessage.toUpperCase().split('');
+    let keyDec = key.toUpperCase().split('');
+    let messageArr = [];
+    let keyArr = [];
+    let keyIndex = 0;
+    let res = [];
+
+    messageDec.forEach((item, index) => {
+      if (this.alphabet.includes(item)) {
+        messageArr.push(this.alphabet.indexOf(item));
+        if (keyDec.length - 1 <= keyIndex) {
+          keyArr.push(this.alphabet.indexOf(keyDec[keyIndex]));
+          keyIndex = 0;
+        } else {
+          keyArr.push(this.alphabet.indexOf(keyDec[keyIndex]));
+          keyIndex = keyIndex + 1;
+        }
+      } else {
+        messageArr.push(messageDec[index]);
+        keyArr.push(messageDec[index]);
+      }
+    })
+
+    messageArr.forEach((item, index) => {
+      if (typeof item === 'number') {
+        let mod = 0;
+        if ((item - keyArr[index]) < 0) {
+          mod = (item - keyArr[index] + 26) % 26;
+        } else {
+          mod = (item - keyArr[index]) % 26;
+        }
+        res.push(this.alphabet[mod]);
+      } else {
+        res.push(item);
+      }
+    })
+    return this.direct ? res.join('') : res.reverse().join('');
   }
 }
 
